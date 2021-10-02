d150e169013c3af8b37a5d260444a5ca07178e8d
Chris Johnson <chrisj645@gmail.com>
2021-10-02 16:21:34 +0100

    1st attempt at a websocket

--- TraderBot/Program.cs
+++ TraderBot/Program.cs
@@ -1,15 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using WebSocketSharp;
 
 namespace TraderBot
 {
-    class Program
+    // JSON Creation Class - start
+    public class Channels
     {
-        static void Main(string[] args)
+        public string name;
+        public string[] product_ids;
+    };
+
+    public class Payload
+    {
+        public string type;
+        public string testcase;
+        public Channels channels;
+    };
+
+    // JSON Creation Class - end
+
+    internal class Program
+    {
+        private static void Main(string[] args)
         {
+            //creating the basic subscibe JSON payload
+            var subscribePayload = new Payload()
+            {
+                type = "subscribe",
+                channels = new Channels
+                {
+                    name = "ticker",
+                    product_ids = new string[] { "BTC-USD" }
+                }
+            };
+
+            // serializing the json object
+            //string subscribe_message = JsonConvert.SerializeObject(subscribePayload,
+            //    Formatting.Indented,
+            //    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            string subscribe_message = JsonConvert.SerializeObject(subscribePayload,
+                new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            //Console.WriteLine(subscribe_message);
+            //Console.ReadLine();
+            string CoinbaseWSS = "wss://ws-feed-public.sandbox.pro.coinbase.com";
+            //Create an instance of a websocket client
+            using (var ws = new WebSocket(CoinbaseWSS))
+            {
+
+                ws.Connect();
+
+
+
+
+
+
+
+
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine("Recieved from the serrver: " + e.Data);
         }
     }
 }
