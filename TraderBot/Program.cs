6ad11412ca35a47a161107251bee30e865ec2cd7
Chris Johnson <chrisj645@gmail.com>
2021-10-08 12:43:25 +0100

    seperate websocket class completed

--- TraderBot/Program.cs
+++ TraderBot/Program.cs
@@ -12,122 +12,26 @@ namespace TraderBot //netflify.com
 {
     class Program
     {
-        #region Fields
-        static WebSocket mSocketClient = null;
-        private readonly string[] defaultmarkets = { "BTC-USD" };
-
-        static int counter = 0;
-        static readonly string subjson = @"
-                {
-                  ""type"": ""subscribe"",
-                  ""channels"": [
-                    {
-                                ""name"": ""ticker"",
-                      ""product_ids"": [
-                        ""BTC-USD""
-                      ]
-                    }
-                  ]
-                }";
-
-        #endregion Fields
-
-        #region Methods
-        private static void InitNetWork()
-        {
-            
-            mSocketClient = new WebSocket("wss://ws-feed-public.sandbox.pro.coinbase.com", sslProtocols: SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
-            mSocketClient.Opened += new EventHandler(MSocketClient_Opened);
-            mSocketClient.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(MSocketClient_Error);
-            mSocketClient.Closed += new EventHandler(MSocketClient_Closed);
-            //mSocketClient.MessageReceived += new EventHandler<WebSocket4Net.MessageReceivedEventArgs>(websocket_MessageReceived);
-            //mSocketClient.DataReceived += new EventHandler<DataReceivedEventArgs>(websocket_DataReceived);
-            
-            mSocketClient.MessageReceived += MSocketClient_MessageReceived;
-            mSocketClient.DataReceived += MSocketClient_DataReceived;
-            //ws.OnMessage += Ws_OnMessage;
-            mSocketClient.Open();
-        }
-
-        #endregion Methods
-
-
         static void Main(string[] args)
         {
-            
-            //InitNetWork();
-            
-            var mJson = new mJson("subscribe", "BTC-USD");
-            Console.WriteLine(mJson.payload());
 
+
+
+            var mJson = new mJson("subscribe", "BTC-USD").payload();
+            Console.WriteLine(mJson);
+
+            var mWebsocket = new mWebsocket(mJson);
+            mWebsocket.InitNetWork();
+            //Console.WriteLine(candles);
+
+
+            //InitNetWork(mJsonString);
             
+
             Console.ReadKey();
         }
 
-        static List<LiveData> candles = new List<LiveData>();
-
-        private static void MSocketClient_Opened(object sender, EventArgs e)
-        {
-            
-            mSocketClient.Send(subjson);
-        }
-
-        private static void MSocketClient_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
-        {
-            Console.WriteLine("websocket_Error:" + e.Exception.ToString());
-        }
-
-        private static void MSocketClient_Closed(object sender, EventArgs e)
-        {
-            Console.WriteLine("Connection closed");
-        }
-
-        private static void MSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
-        {
-            //Console.WriteLine("Message Recieved" + e.Message);
-            if (counter != 0)
-                try
-                {
-                    LiveData rawCandles = JsonConvert.DeserializeObject<LiveData>(e.Message);
-                    Console.WriteLine(rawCandles.Price);
-                    candles.Add(rawCandles);
-                    //Console.WriteLine("Current count is: " + counter);
-                    //Console.WriteLine(candles);
-                    //foreach (LiveData aCandle in candles)
-                    //{
-                    //    Console.WriteLine(aCandle);
-                    //}
-
-
-                    //Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(e.Message);
-                    //Console.WriteLine(values.Count);
-                    //dict works but needs adding to a list now?
-                    //List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
-
-
-
-
-
-
-
-
-
-
-
-                }
-
-                catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            counter++;
-        }
-
-        private static void MSocketClient_DataReceived(object sender, DataReceivedEventArgs e)
-        {
-            Console.WriteLine("DataRecieved" + e.Data);
-            
-        }
+        
 
 
 
--- TraderBot/mWebsocket.cs
+++ TraderBot/mWebsocket.cs
@@ -1,105 +1,103 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Security.Authentication;
-//using WebSocket4Net;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+using WebSocket4Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-//namespace TraderBot
-//{
-//    class mWebsocket
-//    {
-//        #region Fields
-//        static WebSocket mSocketClient = null;
+namespace TraderBot
+{
+    class mWebsocket
+    {
+        public string mPayload;
 
-//        static int counter = 0;
-//        static readonly string subjson = @"
-//                {
-//                  ""type"": ""subscribe"",
-//                  ""channels"": [
-//                    {
-//                                ""name"": ""ticker"",
-//                      ""product_ids"": [
-//                        ""BTC-USD""
-//                      ]
-//                    }
-//                  ]
-//                }";
+        public mWebsocket(string mPayload)
+        {
+            this.mPayload = mPayload;
+        }
 
-//        #endregion Fields
+        WebSocket mSocketClient = null;
+        int counter = 0;
 
-//        #region Methods
-//        private static void InitNetWork()
-//        {
-
-//            mSocketClient = new WebSocket("wss://ws-feed-public.sandbox.pro.coinbase.com", sslProtocols: SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
-//            mSocketClient.Opened += new EventHandler(MSocketClient_Opened);
-//            mSocketClient.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(MSocketClient_Error);
-//            mSocketClient.Closed += new EventHandler(MSocketClient_Closed);
-//            //mSocketClient.MessageReceived += new EventHandler<WebSocket4Net.MessageReceivedEventArgs>(websocket_MessageReceived);
-//            //mSocketClient.DataReceived += new EventHandler<DataReceivedEventArgs>(websocket_DataReceived);
-
-//            mSocketClient.MessageReceived += MSocketClient_MessageReceived;
-//            mSocketClient.DataReceived += MSocketClient_DataReceived;
-//            //ws.OnMessage += Ws_OnMessage;
-//            mSocketClient.Open();
-//        }
-
-//        #endregion Methods
+        
+        
 
 
-//        static void Main(string[] args)
-//        {
-
-//            InitNetWork();
-
-//            Console.ReadKey();
-//        }
-
-//        static List<LiveData> candles = new List<LiveData>();
-
-//        private static void MSocketClient_Opened(object sender, EventArgs e)
-//        {
-
-//            mSocketClient.Send(subjson);
-//        }
-
-//        private static void MSocketClient_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
-//        {
-//            Console.WriteLine("websocket_Error:" + e.Exception.ToString());
-//        }
-
-//        private static void MSocketClient_Closed(object sender, EventArgs e)
-//        {
-//            Console.WriteLine("Connection closed");
-//        }
-
-//        private static void MSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
-//        {
-//            //Console.WriteLine("Message Recieved" + e.Message);
-//            if (counter != 0)
-//                try
-//                {
-//                    LiveData rawCandles = JsonConvert.DeserializeObject<LiveData>(e.Message);
-//                    Console.WriteLine(rawCandles.Price);
-//                    candles.Add(rawCandles);
-//                    //Console.WriteLine("Current count is: " + counter);
-//                    //Console.WriteLine(candles);
-//                    //foreach (LiveData aCandle in candles)
-//                    //{
-//                    //    Console.WriteLine(aCandle);
-//                    //}
+        #region Methods
+        public void InitNetWork()
+        {
+            
 
 
-//                    //Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(e.Message);
-//                    //Console.WriteLine(values.Count);
-//                    //dict works but needs adding to a list now?
-//                    //List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
+            mSocketClient = new WebSocket("wss://ws-feed-public.sandbox.pro.coinbase.com", sslProtocols: SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
+            mSocketClient.Opened += new EventHandler(MSocketClient_Opened);
+            mSocketClient.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(MSocketClient_Error);
+            mSocketClient.Closed += new EventHandler(MSocketClient_Closed);
+            //mSocketClient.MessageReceived += new EventHandler<WebSocket4Net.MessageReceivedEventArgs>(websocket_MessageReceived);
+            //mSocketClient.DataReceived += new EventHandler<DataReceivedEventArgs>(websocket_DataReceived);
+
+            mSocketClient.MessageReceived += MSocketClient_MessageReceived;
+            mSocketClient.DataReceived += MSocketClient_DataReceived;
+            //ws.OnMessage += Ws_OnMessage;
+            mSocketClient.Open();
+        }
+
+        #endregion Methods
 
 
+        //private string InitPayload(mJson mPayload)
+        //{
+        //    string innerpayload = mPayload.ToString();
+        //    return innerpayload;
+            
+        //}
+
+        
+
+        
+
+        private void MSocketClient_Opened(object sender, EventArgs e)
+        {
+
+            mSocketClient.Send(mPayload);
+        }
+
+        private void MSocketClient_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            Console.WriteLine("websocket_Error:" + e.Exception.ToString());
+        }
+
+        private void MSocketClient_Closed(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connection closed");
+        }
+
+        public void MSocketClient_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            List<LiveData> candles = new List<LiveData>();
+            //Console.WriteLine("Message Recieved" + e.Message);
+            if (counter != 0)
+                try
+                {
+                    Console.WriteLine(e.Message);
+                    LiveData rawCandles = JsonConvert.DeserializeObject<LiveData>(e.Message);
+                    Console.WriteLine(rawCandles.Price);
+                    candles.Add(rawCandles);
+                    //Console.WriteLine("Current count is: " + counter);
+                    //Console.WriteLine(candles);
+                    //foreach (LiveData aCandle in candles)
+                    //{
+                    //    Console.WriteLine(aCandle);
+                    //}
+
+
+                    //Dictionary<string, string> values = JsonConvert.DeserializeObject<Dictionary<string, string>>(e.Message);
+                    //Console.WriteLine(values.Count);
+                    //dict works but needs adding to a list now?
+                    //List<Product> products = JsonConvert.DeserializeObject<List<Product>>(json);
 
 
 
@@ -109,30 +107,32 @@
 
 
 
-//                }
-
-//                catch (Exception ex)
-//                {
-//                    Console.WriteLine(ex);
-//                }
-//            counter = counter + 1;
-//        }
-
-//        private static void MSocketClient_DataReceived(object sender, DataReceivedEventArgs e)
-//        {
-//            Console.WriteLine("DataRecieved" + e.Data);
-
-//        }
 
 
+                }
 
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            counter = counter + 1;
+        }
 
+        private void MSocketClient_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine("DataRecieved" + e.Data);
+
+        }
 
 
 
 
-//    }
-//}
+
+
+
+
+    }
+}
 
 
 
