d46c1da428849d43e158e917f2cf60abf5397e88
Chris Johnson <chrisj645@gmail.com>
2021-10-18 21:20:32 +0100

    DB initial Setup complete

---
+++ TraderBot/SqliteDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraderBot
+{
+    class SqliteDataAccess
+    {
+
+        public static List<HistoricalData> LoadData()
+        {
+
+        }
+
+        public static List<HistoricalData> SaveData()
+        {
+
+        }
+
+
+
+    }
+}
